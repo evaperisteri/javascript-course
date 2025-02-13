cceed3fc871ef72e6232ef9210dd07d015f65b0f
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-02-13 21:00:53 +0200

    modules&Browser

---
+++ modules/src/lib2.js
@@ -0,0 +1,11 @@
+//named 
+const PI = 3.14
+const API_URL = "https://codingfactory.aueb.gr"
+const EPSILON = 0.000005
+
+//default exports
+const SEC_PER_MIN = 60
+export default SEC_PER_MIN
+
+//syntax for named exports
+export { PI, EPSILON, API_URL}
